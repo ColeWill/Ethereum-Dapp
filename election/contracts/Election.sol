2812721991d7cf07a45c45490a2fd7b1099e5de7
Cole Williams <cdubs@Coles-MacBook-Pro.local>
2018-06-02 16:21:40 -0600

    initial Dapp is deployed and running

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -7,11 +7,22 @@ contract Election {
         string name;
         uint voteCount;
     }
-    // Store a Candidate
+
+    // store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
     mapping(uint => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
-     
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    
+    
     function Election() public{
         //run whenever our contract is migrated and deployed
         addCandidate('Candidate1');
@@ -22,6 +33,23 @@ contract Election {
         candidatesCount ++; // increments the candidatesCount
         // mapping[key] = instantiate a candidate (id, name, inital vote count of 0);
         candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they have not voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateId);
     }
 }
 
