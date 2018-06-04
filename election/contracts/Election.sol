50ab87857ddc03b7bec827246fddbccfd0e010d6
Cole Williams <cdubs@Coles-MacBook-Pro.local>
2018-06-03 19:37:19 -0600

    testing suite complete

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -9,6 +9,8 @@ contract Election {
     }
 
     // store accounts that have voted
+    // takes an address and returns a boolean if the account
+    // exists in the mapping it returns true, if not it returns false
     mapping(address => bool) public voters;
     // Store Candidates
     // Fetch Candidate
@@ -21,7 +23,7 @@ contract Election {
         uint indexed _candidateId
     );
 
-    
+
     
     function Election() public{
         //run whenever our contract is migrated and deployed
@@ -34,15 +36,17 @@ contract Election {
         // mapping[key] = instantiate a candidate (id, name, inital vote count of 0);
         candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
     }
-
+    // "msg" is meta data that solidity allows the app to handle
+    // "sender" is the account (stored in the meta data)
     function vote (uint _candidateId) public {
-        // require that they have not voted before
+        // require that the acct has not voted before
+        // checks the voters mapping to see itf the acct (msg.sender) is NOT already w/in the mapping
         require(!voters[msg.sender]);
 
-        // require a valid candidate
+        // require a valid candidate is selected (Id# has to exist within the mapping)
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        // record that voter has voted
+        // record that voter has voted, store in 'Voters' mapping on ln14 
         voters[msg.sender] = true;
 
         // update candidate vote count
--- election/test/election.js
+++ election/test/election.js
@@ -5,7 +5,7 @@ var electionInstance
 contract("Election", function(accounts){
     // see that contract was intitialized with the correct number of candidates
 
-    it('initializes with two candidates', function(){
+    it('initializes with two candidates ln8', function(){
         // fetch an instance of the deployed contract
         return Election.deployed().then(function(instance){
             return instance.candidatesCount();
@@ -15,7 +15,7 @@ contract("Election", function(accounts){
         });
     });
 
-    it("it initializes the candidates with the correct values", function(){
+    it("it initializes the candidates with the correct values ln18", function(){
         return Election.deployed().then(function(instance){
             electionInstance = instance;
             return electionInstance.candidates(1);
@@ -29,5 +29,73 @@ contract("Election", function(accounts){
             assert.equal(candidate[1], "Candidate2", "contains the correct name");
             assert.equal(candidate[2], 0, "contains the correct votes count");
         });
+    });
+    // check to see that vote increments by 1
+    // check to make sure the account is added to the voters mapping
+    it("allows a voter to cast a vote ln35", function(){
+        // gets a copy of deployed contract
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            candidateId = 1;
+            // we pass in the app meta data and the candidate id
+            return electionInstance.vote(candidateId, {from: accounts[0] });
+        }).then(function(receipt){
+            // read the voter's mapping and reads the account mapping
+            return electionInstance.voters(accounts[0]);
+            // returns the boolean value to see if the account has voted
+        }).then(function(voted){
+            assert(voted, "the voter was marked as voted");
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate){
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "increments the candidate's vote count");
+        });
+    });
+
+    it("throws an exception for invalid candidates ln55", function(){
+        // fetch an instance of our app
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            // vote 1x for candidate 99
+            return electionInstance.vote(99, { from: accounts[1] })
+            // check for an error and pass it into the callback function 
+        }).then(assert.fail).catch(function(error) {
+            // checks the error message for the sbstr "revert" --> indexOf is a number for the first char position of "revert" w/in the error message
+            assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+            return electionInstance.candidates(1);
+        }).then(function(candidate1){
+            var voteCount = candidate1[2];
+            // assert that vote count is still 1 (we already voted 1x cor cand1 earlier in test)
+            assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+            return electionInstance.candidates(2);
+        }).then(function(candidate2){
+            var voteCount = candidate2[2];
+            // assert that cand2 still does not have any votes
+            assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+        });
+    it("throws an exception for double voting ln74", function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            candidateId = 2;
+            // give candidate 2 its first vote
+            electionInstance.vote(candidateId, {from: accounts[1] });
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate){
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "accepts first vote");
+            // try to cote for candidate 2 again  
+            return electionInstance.vote(candidateId, { from: accounts[1] });
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+            return electionInstance.candidates(1);
+        }).then(function(candidate1){
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "Candidate 1 did not get new votes");
+            return electionInstance.canddiates(2);
+        }).then(function(candidate2){
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 1 , "candidate 2 did not get new votes");
+        });
+    });   
     });
 });
